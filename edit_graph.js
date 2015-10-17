09e5e1a1d1bf8f14db8d2494925ed3a8693c749a
rooooooooob <raftias@gmail.com>
2015-10-16 18:07:41 -0700

    refactored code into multiple files

---
+++ edit_graph.js
@@ -0,0 +1,51 @@
+function removeArc(vertices, u, v)
+{
+	var neighbors = vertices[u].edges;
+	const vindex = neighbors.indexOf(v);
+	neighbors[vindex] = neighbors[neighbors.length - 1];
+	neighbors.pop();
+}
+
+function removeEdge(vertices, u, v)
+{
+	removeArc(vertices, u, v);
+	removeArc(vertices, v, u);
+}
+
+function removeVertex(vertices, id)
+{
+	const lastID = vertices.length - 1;
+	for (var property in vertices[id])
+	{
+		vertices[id][property] = vertices[lastID][property];
+	}
+	vertices.pop();
+	for (var vid = 0; vid < vertices.length; ++vid)
+	{
+		const neighbors = vertices[vid].edges;
+		for (var i = 0; i < neighbors.length; ++i)
+		{
+			if (neighbors[i] == id)
+			{
+				neighbors[i] = neighbors[neighbors.length - 1];
+				neighbors.pop();
+			}
+			else if (neighbors[i] == lastID)
+			{
+				neighbors[i] = id;
+			}
+		}
+	}
+}
+
+function addEdge(vertices, u, v)
+{
+	if (vertices[u].edges.indexOf(v) == -1)
+	{
+		vertices[u].edges.push(v);
+	}
+	if (!directed && vertices[v].edges.indexOf(u) == -1)
+	{
+		vertices[v].edges.push(u);
+	}
+}
---
+++ main.js
@@ -0,0 +1,147 @@
+var canvas;
+var context;
+var vertices = [];
+var vertexSize = 16;
+var selectedVertex = -1;
+var directed = false;
+const Modes = {
+	INSERT : "insert",
+	REMOVE : "remove"
+};
+var mode = Modes.INSERT;
+
+
+function initialise()
+{
+	canvas = document.getElementById("myCanvas");
+	context = canvas.getContext("2d");
+	canvas.addEventListener("mousedown", onMouseClick, false);
+	redraw();
+}
+
+function redraw()
+{
+	context.fillStyle = "#DDDDDD";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	// draw edges
+	for (var id = 0; id < vertices.length; ++id)
+	{
+		const vertex = vertices[id];
+		const neighbors = vertex.edges;
+		for (var i = 0; i < neighbors.length; ++i)
+		{
+			const neighbor = vertices[neighbors[i]];
+			context.beginPath();
+			context.lineWidth = 3;
+			context.moveTo(vertex.x, vertex.y);
+			context.lineTo(neighbor.x, neighbor.y);
+			context.stroke();
+		}
+	}
+	//draw vertices on top
+	for (var id = 0; id < vertices.length; ++id)
+	{
+		const vertex = vertices[id];
+		context.beginPath();
+		context.arc(vertex.x, vertex.y, vertexSize, 0, 2*Math.PI);
+		context.fillStyle = (id == selectedVertex ? "#555555" : "#BBBBBB");
+		context.fill();
+		context.lineWidth = 3;
+		context.strokeStyle = "#000000";
+		context.stroke();
+		context.font = "12px Arial";
+		context.lineWidth = 1;
+		context.textAlign = "center";
+		context.textBaseline = "middle";
+		context.fillStyle = "#000000";
+		context.fillText(id, vertex.x, vertex.y);
+	}
+}
+
+function getVertexAt(x, y)
+{
+	for (var id = 0; id < vertices.length; ++id)
+	{
+		const vertex = vertices[id];
+		const difX = vertex.x - x;
+		const difY = vertex.y - y;
+		if (Math.sqrt(difX*difX + difY*difY) <= vertexSize)
+		{
+			return id;
+		}
+	}
+	return -1;
+}
+
+function onMouseClick(event)
+{
+	const x = event.clientX - canvas.getBoundingClientRect().left;
+	const y = event.clientY - canvas.getBoundingClientRect().top;
+	const vertexClickedOn = getVertexAt(x, y);
+	// didn't click on vertex -> create new vertex
+	if (vertexClickedOn == -1)
+	{
+		switch (mode)
+		{
+		case Modes.INSERT:
+			vertices.push({x:x, y:y, edges:[]});
+			if (selectedVertex != -1)
+			{
+				addEdge(vertices, selectedVertex, vertices.length - 1);
+			}
+			break;
+		case Modes.REMOVE:
+			selectedVertex = -1;
+			break;
+		}
+	}
+	else // selected a vertex -> select or create edge between previous selected vertex
+	{
+		if (selectedVertex != -1)
+		{
+			// no self-loops
+			if (vertexClickedOn != selectedVertex)
+			{
+				switch (mode)
+				{
+				case Modes.INSERT:
+					addEdge(vertices, selectedVertex, vertexClickedOn);
+					break;
+				case Modes.REMOVE:
+					removeEdge(vertices, selectedVertex, vertexClickedOn);
+					break;
+				}
+			}
+			else // delete vertex
+			{
+				if (mode == Modes.REMOVE)
+				{
+					removeVertex(vertices, selectedVertex);
+				}
+			}
+			selectedVertex = -1;
+		}
+		else
+		{
+			selectedVertex = vertexClickedOn;
+		}
+	}
+	
+	
+	redraw();
+}
+
+function setMode(newMode)
+{
+	mode = newMode;
+}
+
+function setModeToInsert()
+{
+	setMode(Modes.INSERT);
+}
+
+function setModeToRemove()
+{
+	setMode(Modes.REMOVE);
+}
